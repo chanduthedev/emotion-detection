d0467c41ade2c16e288eece6261a6a517bc5c1d8
Chandu <chanduthedev@gmail.com>
2021-10-02 13:55:58 +0800

    Removed default react settings from html home page.

--- emotion_analysis_ui/src/services/serviceCalls.js
+++ emotion_analysis_ui/src/services/serviceCalls.js
@@ -17,7 +17,6 @@ async function predictService(inputText, modelName) {
 
 async function getTrainedModelService() {
   const apiEndPoint = `https://chemotionanalysis.herokuapp.com/models`;
-  // const apiEndPoint = `http://192.168.10.134:9898/models`;
   const serverResp = await fetch(apiEndPoint, {
     method: "GET",
   })
